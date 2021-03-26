11e471a7a7859bc75a1f0068104d330df681f78c
Yasas770 <64302398+Yasas770@users.noreply.github.com>
2021-03-26 19:44:57 +0530

    Styles Added
    Classic Footer with hr tags

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -4,7 +4,7 @@ import Row from './Row.js';
 import requests from './requests'; 
 import Banner from './Banner.js';
 import Nav from './Nav.js';
-import footer from './footer.js';
+import Footer from './footer.js';
 
 
 
@@ -18,14 +18,22 @@ function App() {
     <Banner> </Banner>
     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
     isLargeRow></Row>
+     <hr className="breaker"></hr>
     <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+    <hr className="breaker"></hr>
     <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
+    <hr className="breaker"></hr>
     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
+    <hr className="breaker"></hr>
     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
+    <hr className="breaker"></hr>
     <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
+    <hr className="breaker"></hr>
     <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
+    <hr className="breaker"></hr>
     <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
-    <footer></footer>
+    <hr className="breaker"></hr>
+    <Footer></Footer>
     </div>
   );
 }
--- netflix-clone/src/footer.js
+++ netflix-clone/src/footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import './footer.css';
 
 
 const footer = () => {
     return (
-<div>
+<div className="Main_Footer">
+    <div className="container">
+        <div className="row">
+        <img className="footer_logo"  src="Assets/netflix_logo.png"  alt="Netflix Logo" />
+        <h3 className="name">Yasas_17</h3>
+                
+        </div>
+    </div>
 
-    
+
+
 </div>
 
     )
 }
+
+
+export default footer;
